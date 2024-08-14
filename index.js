4b8c4fe63a811dc8273a1e2d8aa244d409f6769b
Anirach <Anirach.m@fitm.kmutnb.ac.th>
2024-08-14 11:01:23 +0700

    checkport

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
